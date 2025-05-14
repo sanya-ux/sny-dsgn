ed1e4f2e8c831b92dc0a72a715b8f179d5f42308
sanya-ux <sanya-ux@users.noreply.github.com>
2025-05-14 07:20:58 +0000

    deploy: 04e6c21adc5d492cb9bb8abbe18c5f67ae1432b4

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747206897',resources: [
+    cacheName: 'chirpy-1747207257',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
